fa7bd31e9dd1c00078451cb95f7ec3ec9cba8ac0
josue20651-dev <josue20651@gmail.com>
2026-02-21 22:16:59 -0300

    cree un environment para llamar a la url correcta al ocupar flow ya sea en local o ya una vez hosteado en railway

--- src/app/services/flow.ts
+++ src/app/services/flow.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Flow {
-  private api = 'http://localhost:3000';
+  private api = environment.apiUrl;
 
   constructor(private http: HttpClient){}
 
