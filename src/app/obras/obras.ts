9e61e66e1fd74d591008a3a8fc8d6c20c99bd583
josue20651-dev <josue20651@gmail.com>
2026-03-18 18:55:01 -0300

    quitamos el correo y telefono del cliente en el apartado de contacto y cambiamos los precios

--- src/app/obras/obras.ts
+++ src/app/obras/obras.ts
@@ -11,7 +11,7 @@ export class Obras {
   obras = [
     {
       titulo: 'El Lobo y la Luna',
-      subtitulo: 'Novela · Romance & Drama',
+      subtitulo: 'Novela · Romance & Drama · Edición Fisica',
       anio: '2025',
       descripcion: 'Hay una leyenda que dice que cuando un hombre mira la luna, mira a la mujer que ama. En los caminos húmedos del sur de Chile, una casa llena de libros guarda un cofre con cartas, poemas y fotografías: fragmentos de un amor que parecía absoluto.',
       precio: '9,99 USD',
